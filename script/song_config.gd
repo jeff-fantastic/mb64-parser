c9507b91e0b12556f02a2eed505c42d8fb0e03cb
jefftastic <48529067+jeff-fantastic@users.noreply.github.com>
2024-05-23 10:01:00 -0400

    copy over music arrays from 1.0.0

--- script/song_config.gd
+++ script/song_config.gd
@@ -5,6 +5,139 @@ class_name SongConfig extends Window
 Configues the song selection for a level
 '''
 
+## 1.0.0's list of music names
+const music : Array[String] = [
+	## Vanilla, 14 tracks
+	"Bob-omb Battlefield",
+	"Slider",
+	"Dire, Dire Docks",
+	"Dire, Dire Docks (Underwater)",
+	"Lethal Lava Land",
+	"Cool, Cool Mountain",
+	"Big Boo's Haunt",
+	"Hazy Maze Cave",
+	"Hazy Maze Cave (Haze)",
+	"Koopa's Road",
+	"Stage Boss",
+	"Koopa's Theme",
+	"Ultimate Koopa",
+	"Inside the Castle Walls",
+	
+	## BTCM, also 14 tracks
+	"Cosmic Castle",
+	"Red-Hot Reservoir",
+	"Lonely Floating Farm",
+	"Jurassic Savanna",
+	"The Phantom Strider",
+	"Virtuaplex",
+	"Immense Residence",
+	"Thwomp Towers",
+	"Cursed Boss",
+	"Road To The Boss",
+	"Urbowser",
+	"The Show's Finale",
+	"Parasite Moon",
+	"AGAMEMNON",
+	
+	## Imports, 68 tracks
+	"Bianco Hills (Super Mario Sunshine)",
+	"Sky and Sea (Super Mario Sunshine)",
+	"Secret Course (Super Mario Sunshine)",
+	"Comet Observatory (Mario Galaxy)",
+	"Buoy Base Galaxy (Mario Galaxy)",
+	"Battlerock Galaxy (Mario Galaxy)",
+	"Ghostly Galaxy (Mario Galaxy)",
+	"Purple Comet (Mario Galaxy)",
+	"Honeybloom Galaxy (Mario Galaxy 2)",
+	"Piranha Creeper Creek (3D World)",
+	"Desert (New Super Mario Bros.)",
+
+	"Koopa Troopa Beach (Mario Kart 64)",
+	"Frappe Snowland (Mario Kart 64)",
+	"Bowser's Castle (Mario Kart 64)",
+	"Rainbow Road (Mario Kart 64)",
+	"Waluigi Pinball (Mario Kart DS)",
+	"Rainbow Road (Mario Kart 8)",
+
+	"Mario's Pad (Super Mario RPG)",
+	"Nimbus Land (Super Mario RPG)",
+	"Forest Maze (Super Mario RPG)",
+	"Sunken Ship (Super Mario RPG)",
+
+	"Dry Dry Desert (Paper Mario 64)",
+	"Forever Forest (Paper Mario 64)",
+	"Petal Meadows (Paper Mario: TTYD)",
+	"Riddle Tower (Paper Mario: TTYD)",
+	"Rogueport Sewers (Paper Mario: TTYD)",
+	"X-Naut Fortress (Paper Mario: TTYD)",
+	"Flipside (Super Paper Mario)",
+	"Lineland Road (Super Paper Mario)",
+	"Sammer Kingdom (Super Paper Mario)",
+	"Floro Caverns (Super Paper Mario)",
+	"Overthere Stair (Super Paper Mario)",
+
+	"Yoshi's Tropical Island (Mario Party)",
+	"Rainbow Castle (Mario Party)",
+	"Behind Yoshi Village (Partners in Time)",
+	"Gritzy Desert (Partners in Time)",
+	"Bumpsy Plains (Bowser's Inside Story)",
+	"Deep Castle (Bowser's Inside Story)",
+
+	"Overworld (Yoshi's Island)",
+	"Underground (Yoshi's Island)",
+	"Title (Yoshi's Story)",
+
+	"Kokiri Forest (Ocarina of Time)",
+	"Lost Woods (Ocarina of Time)",
+	"Gerudo Valley (Ocarina of Time)",
+	"Stone Tower Temple (Majora's Mask)",
+	"Outset Island (Wind Waker)",
+	"Lake Hylia (Twilight Princess)",
+	"Gerudo Desert (Twilight Princess)",
+	"Skyloft (Skyward Sword)",
+
+	"Frantic Factory (Donkey Kong 64)",
+	"Hideout Helm (Donkey Kong 64)",
+	"Creepy Castle (Donkey Kong 64)",
+	"Gloomy Galleon (Donkey Kong 64)",
+	"Fungi Forest (Donkey Kong 64)",
+	"Crystal Caves (Donkey Kong 64)",
+	"Angry Aztec (Donkey Kong 64)",
+	"In a Snow-Bound Land (DKC 2)",
+
+	"Bubblegloop Swamp (Banjo-Kazooie)",
+	"Freezeezy Peak (Banjo-Kazooie)",
+	"Gobi's Valley (Banjo-Kazooie)",
+
+	"Factory Inspection (Kirby 64)",
+	"Green Garden (Bomberman 64)",
+	"Black Fortress (Bomberman 64)",
+	"Windy Hill (Sonic Adventure)",
+	"Sky Tower (Pokemon Mystery Dungeon)",
+	"Youkai Mountain (Touhou 10)",
+	"Forest Temple (Final Fantasy VII)",
+	# "Band Land is love, Band Land is life" I say, shovelling dry ass chalk 
+	# into my mouth, similar to that of a derranged lunatic
+	"Band Land (Rayman)", 
+	
+	## Retro, 7 tracks
+	"Overworld (Super Mario Bros.)",
+	"Castle Mix (Super Mario Bros.)",
+	"Overworld (Super Mario Bros. 2)",
+	"Overworld Mix (Super Mario Bros. 3)",
+	"Fortress (Super Mario Bros. 3)",
+	"Athletic (Super Mario World)",
+	"Castle (Super Mario World)",
+]
+
+## Category names
+const category : Array[String] = [
+	"Super Mario 64 OST",
+	"Beyond the Cursed Mirror OST",
+	"ROM Hack Music Ports",
+	"Retro 2D Mario Music"
+]
+
 ## Current [MB64Level] resource
 var current_res : MB64Level :
 	set(value) : current_res = value; prepare_backup(); update_fields()
